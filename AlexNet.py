51d2acba0af16d606495450c69e8cbb22aa2fc10
zhangzw16 <thuzhangzw@outlook.com>
2018-03-13 00:20:27 +0800

    adjust load_datasets

--- AlexNet.py
+++ AlexNet.py
@@ -1,37 +1,39 @@
+import keras.backend as K
+import matplotlib.pyplot as plt
 import numpy as np
 from keras import layers
-from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
-from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.applications.imagenet_utils import preprocess_input
+from keras.layers import (Activation, AveragePooling2D, BatchNormalization,
+                          Conv2D, Dense, Dropout, Flatten,
+                          GlobalAveragePooling2D, GlobalMaxPooling2D, Input,
+                          MaxPooling2D, ZeroPadding2D)
 from keras.models import Model
 from keras.preprocessing import image
-from keras.utils import layer_utils
+from keras.utils import layer_utils, plot_model
 from keras.utils.data_utils import get_file
-from keras.applications.imagenet_utils import preprocess_input
 from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
+from matplotlib.pyplot import imshow
 from utils import *
 
-import keras.backend as K
 K.set_image_data_format('channels_last')
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_datasets()
+# X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_datasets()
+X_train_orig, Y_train_orig = load_datasets()
 
 # Normalize image vectors
 X_train = X_train_orig/255.
-X_test = X_test_orig/255.
+# X_test = X_test_orig/255.
 
 # Reshape
 Y_train = Y_train_orig.T
-Y_test = Y_test_orig.T
+# Y_test = Y_test_orig.T
 
 print ("number of training examples = " + str(X_train.shape[0]))
-print ("number of test examples = " + str(X_test.shape[0]))
+# print ("number of test examples = " + str(X_test.shape[0]))
 print ("X_train shape: " + str(X_train.shape))
 print ("Y_train shape: " + str(Y_train.shape))
-print ("X_test shape: " + str(X_test.shape))
-print ("Y_test shape: " + str(Y_test.shape))
+# print ("X_test shape: " + str(X_test.shape))
+# print ("Y_test shape: " + str(Y_test.shape))
 
 def HappyModel(input_shape):
     """
@@ -77,13 +79,14 @@ happyModel = HappyModel((64,64,3))
 
 # 需要修改loss_function
 happyModel.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
+# categorical_crossentropy
 
 happyModel.fit(x=X_train, y=Y_train, epochs=10, batch_size=20)
 
-preds = happyModel.evaluate(x=X_test, y=Y_test,)
+# preds = happyModel.evaluate(x=X_test, y=Y_test,)
 
 print()
-print ("Loss = " + str(preds[0]))
-print ("Test Accuracy = " + str(preds[1]))
+# print ("Loss = " + str(preds[0]))
+# print ("Test Accuracy = " + str(preds[1]))
 
 happyModel.summary()
--- utils.py
+++ utils.py
@@ -8,17 +8,22 @@ def mean_pred(y_true, y_pred):
     return K.mean(y_pred)
 
 def load_datasets():
-    train_dataset = h5py.File('datasets/train_happy.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_dataset = h5py.File('./datasets/codes.hdf5', "r")
+    train_set_x_orig = np.array(train_dataset["images"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["codename"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/test_happy.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    # test_dataset = h5py.File('datasets/test_happy.h5', "r")
+    # test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    # test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    # classes = np.array(test_dataset["list_classes"][:]) # the list of classes
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    # train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    # test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    # return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_orig, train_set_y_orig
+
+if __name__ == '__main__':
+    x, y = load_datasets()
+    print(y[2])
