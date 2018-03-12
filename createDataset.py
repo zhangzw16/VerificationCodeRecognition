d91c7890e5d19431c0720359c00409f3c342ee1b
zhangzw16 <thuzhangzw@outlook.com>
2018-03-12 23:24:10 +0800

    h5 file update

--- createDataset.py
+++ createDataset.py
@@ -1,15 +1,21 @@
 import h5py
+from PIL import Image
+import numpy as np
 
-def createTrain():
-    f=h5py.File("./datasets/codes.hdf5","r")
-    dset = f['codename']
+def createTrainSet():
+    trainList = []
+    f=h5py.File("./datasets/codes.hdf5","r+")
+    dset_code = f['codename']
     # print(dset.shape)
-    for i in dset.value:
-        print(i.decode())
+    for i in dset_code.value:
+        # print(i.decode())
         name = i.decode()
-        
-
+        img = Image.open("./images/{}.jpg".format(name))
+        imgMatrix = np.array(img)
+        trainList.append(imgMatrix)
+    # print(trainList[1].shape)
+    f.create_dataset('images', data=trainList)
 
 
 if __name__ == '__main__':
-    createTrain()
+    createTrainSet()
---
+++ test.py
@@ -0,0 +1,16 @@
+import h5py
+from PIL import Image
+
+f = h5py.File("./datasets/codes.hdf5","r")
+dset_code = f['codename']
+for i in dset_code.value:
+    # print(i.decode())
+    name = i.decode()
+    print(name)
+
+
+dset_image = f['images']
+img = dset_image.value[0]
+img = Image.fromarray(img)
+img.show()
+print(img)
